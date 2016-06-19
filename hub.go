76b6a638e9d607460161f7d0f8faef9605c90967
tirinox <tirinox@gmail.com>
2016-06-19 11:06:08 +0200

    close connection after publish (other way)

--- hub.go
+++ hub.go
@@ -43,7 +43,7 @@ func (h *Hub) sendMessage(m *PushMessage) {
 		sent = true
 	}
 	m.sender.WritePublishResult(sent, m.receiverId)
-	m.sender.close()
+	m.sender.connection.Close()
 }
 
 func (h *Hub) Run() {
