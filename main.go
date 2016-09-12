adf052a6840d96bdd7e69c4fa0b48ae1761601b9
tirinox <tirinox@gmail.com>
2016-09-12 20:15:10 +0400

    no log for now

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+)
+
 func main() {
 	// if you want to turn logging off
-	// log.SetOutput(ioutil.Discard)
+	log.SetOutput(ioutil.Discard)
 	RunPushApp()
 }
